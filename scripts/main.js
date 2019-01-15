95563350fc1d6d5109c1a0f43f521c52eaffabb9
KATIE SEGAL <KATIE_SEGAL@STUDENT.HKS.HARVARD.EDU>
2019-01-14 23:42:03 -0500

    Adds Katie's visualization homework

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-<script>
-
 var margin = {top: 20, right: 50, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -96,5 +94,3 @@ d3.tsv("data.tsv", function(error, data) {
     focus.select("text").text(formatCurrency(d.close));
   }
 });
-
-</script>
